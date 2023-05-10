2fc70149450982f87f3566311b54e1e170d9645c
William Lefebvre <lefebvre.william82@gmail.com>
2023-05-10 15:50:24 +0200

    optimisation dé

---
+++ src/main/java/leverger/contenu/VueDé.java
@@ -0,0 +1,43 @@
+package leverger.contenu;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+
+public class VueDé {
+
+	public Dé dé;
+	public StackPane paneDé;
+	public Circle cercle;
+	public Polygon triangle;
+	
+	
+	public VueDé(Dé dé, Color couleur) {
+		this.dé = dé;
+        this.cercle = generationCercle(couleur);
+		this.triangle = generationTriangle();
+        this.paneDé = new StackPane();
+        this.paneDé.getChildren().addAll(triangle, cercle);
+	}
+	
+	public Circle generationCercle(Color couleur) {
+		Circle cercle = new Circle(45, couleur);
+		cercle.setTranslateY(30);
+		return cercle;
+	}
+	
+	public Polygon generationTriangle() {
+		Polygon triangle = new Polygon();
+        triangle.getPoints().addAll(new Double[] {
+                100.0, 20.0,
+                0.0, 200.0,
+                200.0, 200.0
+            });
+        triangle.setStroke(Color.BLACK);
+        triangle.setStrokeWidth(5);
+        triangle.setFill(Color.WHITE);
+        return triangle;
+	}
+	//CREER UN SETTER DE PANEDE POUR CHANGER LA COULEUR DU CERCLE 
+}	
