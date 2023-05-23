21c71393893be0e7c8d845d961c1bfabd3fcc481
William Lefebvre <lefebvre.william82@gmail.com>
2023-05-23 21:47:26 +0200

    ajout images

---
+++ src/main/java/leverger/contenu/Panier.java
@@ -0,0 +1,20 @@
+package leverger.contenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.image.Image;
+
+public class Panier {
+    public ArrayList<Fruit> fruits;
+    public Image haut;
+    public Image bas;
+
+
+    public Panier() {
+        this.fruits = new ArrayList<Fruit>();
+        this.haut = new Image("file:img/HautDuPanier.png");
+        this.bas = new Image("file:img/BasDuPanier.png");
+        }
+
+    } 
